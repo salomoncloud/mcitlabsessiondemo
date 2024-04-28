6570e26115c8987669722940ee4ad637cddc3e33
salomoncloud <158000174+salomoncloud@users.noreply.github.com>
2024-04-28 11:03:19 -0400

    Update locals.tf

--- locals.tf
+++ locals.tf
@@ -1,6 +1,19 @@
 locals {
 servicename="abracadabra"
+forum="abracadabramcit"
+
+lengthsa=length(local.servicename)
+length=length(local.forum)
 }
 output "print" {
 value = local.servicename
+}
+output "secondprint" {
+value = local.forum
+}
+output "lengthsa" {
+value = local.lengthsa
+}
+output "lengthforum" {
+value = local.lengthforum
 }
