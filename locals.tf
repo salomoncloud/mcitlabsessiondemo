ae9334b04b0fcb2eca4b7af2579c8ed32dd3274c
salomoncloud <158000174+salomoncloud@users.noreply.github.com>
2024-04-29 15:51:59 -0400

    Update locals.tf

--- locals.tf
+++ locals.tf
@@ -3,19 +3,11 @@ name="salomon"
 lastname="lubin"
 city="montreal"
 landmark="BellCenter"
-landmark2="OldForum"
-landmark3="PontJacqueCartier"
-landmark4="ChezMaTanteHotDogs"
-landmark5="MCIT"
 
 lengthsa=length(local.name)
 length=length(local.lastname)
 lengthcity=length(local.city)
 lengthbell=length(local.landmark)
-lengthforum=length(local.forum)
-lengthpont=length(local.pont)
-lengthhd=length(local.hd)
-lengthmcit=length(local.mcit)
 }
 output "print" {
 value = local.name
@@ -25,18 +17,6 @@ value = local.lastname
 }
 output "printcity" {
 value = local.city
-}
-output "printlandmark2" {
-value = local.landmark2
-}
-output "printlandmark3" {
-value = local.landmark3
-}
-output "printlandmark4" {
-value = local.landmark4
-}
-output "printlandmark5" {
-value = local.landmark5
 }
 output "lengthsa" {
 value = local.lengthsa
@@ -49,16 +29,4 @@ value = local.lengthcity
 }
 output "lengthbell" {
 value = local.landmark
-}
-output "lengthforum" {
-value = local.forum
-}
-output "lengthpont" {
-value = local.pont
-}
-output "lengthhd" {
-value = local.hd
-}
-output "lengthmcit" {
-value = local.mcit
 }
