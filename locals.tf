7f5389bd4a7038be9557bf8de3f8e8cf69c05ba1
salomoncloud <158000174+salomoncloud@users.noreply.github.com>
2024-05-01 17:10:47 -0400

    Update locals.tf

--- locals.tf
+++ locals.tf
@@ -1,29 +1,30 @@
 locals {
-test="test-one-two"
-name="salomon"
-lastname="lubin"
-city="montreal"
-landmark="BellCenter"
-landmark2="Forum"
-landmark3="poutine"
-landmark4="justepourrire"
-landmark5="maplesyrup"
-winterListOfSports=["hockey", "Skiing", "figure skating"]
-universityListOfSchools=["Mcgill", "Concordia", "ETS"]
-montrealListOfRestaurants=["Baroco", "Petes Smoke Meat", "Joe Beef", "Chez Ma Tante", "Taco Trunp"]
-total_output = ["150", "150", "150"]
-listOfNumbers=["4", "5", "1", "2", "3", "4", "5"]
+  test = "test-one-two"
+  name = "salomon"
+  lastname = "lubin"
+  city = "montreal"
+  landmark = "BellCenter"
+  landmark2 = "Forum"
+  landmark3 = "poutine"
+  landmark4 = "justepourrire"
+  landmark5 = "maplesyrup"
+  winterListOfSports = ["hockey", "Skiing", "figure skating"]
+  universityListOfSchools = ["Mcgill", "Concordia", "ETS"]
+  montrealListOfRestaurants = ["Baroco", "Petes Smoke Meat", "Joe Beef", "Chez Ma Tante", "Taco Trunp"]
+  total_output = ["150", "150", "150"]
+  listOfNumbers = ["4", "5", "1", "2", "3", "4", "5"]
 
-lengthtest=length(local.test)
-lengthsa=length(local.name)
-length=length(local.lastname)
-lengthcity=length(local.city)
-lengthbell=length(local.landmark)
-lengthforum=length(local.landmark2)
-lengthpoutine=length(local.landmark3)
-lengthjustepourrire=length(local.landmark4)
-lengthmaplesyrup=length(local.landmark5)
+  lengthtest = length(local.test)
+  lengthsa = length(local.name)
+  length = length(local.lastname)
+  lengthcity = length(local.city)
+  lengthbell = length(local.landmark)
+  lengthforum = length(local.landmark2)
+  lengthpoutine = length(local.landmark3)
+  lengthjustepourrire = length(local.landmark4)
+  lengthmaplesyrup = length(local.landmark5)
 }
+
 output "sumoflistofnumbers"{
 value=sum([for numberoutput in local.listOfNumbers: tonumber(numberoutput)])
 }
@@ -58,32 +59,41 @@ output "printcity" {
 value = local.city
 }
 output "lengthtest" {
-value = local.test
+  value = local.lengthtest
 }
+
 output "lengthsa" {
-value = local.lengthsa
+  value = local.lengthsa
 }
+
 output "length" {
-value = local.length
+  value = local.length
 }
+
 output "lengthcity" {
-value = local.lengthcity
+  value = local.lengthcity
 }
+
 output "lengthbell" {
-value = local.landmark
+  value = local.lengthbell
 }
+
 output "lengthforum" {
-value = local.landmark2
+  value = local.lengthforum
 }
+
 output "lengthpoutine" {
-value = local.landmark3
+  value = local.lengthpoutine
 }
+
 output "lengthjustepourrire" {
-value = local.landmark4
+  value = local.lengthjustepourrire
 }
+
 output "lengthmaplesyrup" {
-value = local.landmark5
+  value = local.lengthmaplesyrup
 }
+
 output "company_name_output"{
 value=var.company_name
 }
