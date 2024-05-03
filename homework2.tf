a15db9931152112768ba57e33f942fe0b39493db
salomoncloud <158000174+salomoncloud@users.noreply.github.com>
2024-05-02 21:10:19 -0400

    Update homework2.tf

--- homework2.tf
+++ homework2.tf
@@ -1,6 +1,20 @@
 variable "country_list" {
   default = ["Canada", "Portugal","India","Netherlands", "Namibia"]
 }
+
 output "join_countries" {
   value = join("+", var.country_list)
+}
+
+locals {
+  contains_keyword = contains(split(" ", var.country_list), "Canada")
+}
+
+
+output "string_contains_no_canada" {
+  value = local.contains_keyword ? "String contains 'Canada'" : "String does not contain 'Canada'"
+}
+
+output "string_contains_canada" {
+  value = local.contains_keyword ? "String contains 'Canada'" : "String does not contain 'Canada'"
 }
