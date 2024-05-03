eaabe464f773abcc0d079ecc8364b701f2c45bd4
salomoncloud <158000174+salomoncloud@users.noreply.github.com>
2024-05-02 21:04:32 -0400

    Update homework2.tf

--- homework2.tf
+++ homework2.tf
@@ -1,1 +1,9 @@
-
+variable "string_list" {
+  default = ["Canada", "Portugal","India","Netherlands", "Namibia"]
+}
+output "join_example" {
+  value = join("+", var.string_list)
+}
+locals {
+  contains_keyword = contains(split(" ", var.original_string), "Canada")
+}
