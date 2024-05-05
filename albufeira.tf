b5bcf68a8cebb4bbfe7240ac3d653b8fa2d25060
salomoncloud <158000174+salomoncloud@users.noreply.github.com>
2024-05-05 16:48:19 +0100

    Update albufeira.tf

--- albufeira.tf
+++ albufeira.tf
@@ -11,6 +11,11 @@ variable "nested_map" {
   }
 }
 locals{
+  characters = ["luke", "yoda", "darth"]
+  enemies_destroyed = [4252, 900, 20000056894]
+  character_enemy_map =   { for index,character in local.characters: # Convert character list to a set
+      character => local.enemies_destroyed[index]
+
   simple_local_flattened_list = flatten(var.simple_nested_list)
   flattened_list = flatten(var.simple_nested_list)
   flattened_map  = flatten([for k, v in var.nested_map : [for kk, vv in v : { group = k, key = kk, value = vv }]])
