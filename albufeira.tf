b6a69cd3e04e76fca9d0f6a706b7cce0bbec9854
salomoncloud <158000174+salomoncloud@users.noreply.github.com>
2024-05-05 15:43:10 +0100

    Update albufeira.tf

--- albufeira.tf
+++ albufeira.tf
@@ -1,1 +1,16 @@
+variable "simple_nested_list" {
+  type    = list(list(string))
+  default = [["a", "b"], ["c", "d"], ["e", "f"]]
+}
+variable "nested_list" {
+  type    = list(list(string))
+  default = [["a", "b"], ["c", "d"], ["e", "f"]]
+}
 
+variable "nested_map" {
+  type    = map(map(string))
+  default = {
+    group1 = {"name" = "Alice", "age" = "25"}
+    group2 = {"name" = "Bob", "age" = "30"}
+  }
+}
