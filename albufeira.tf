673b689926ae378d7afbafcb743ab0d306597810
salomoncloud <158000174+salomoncloud@users.noreply.github.com>
2024-05-05 16:17:56 +0100

    Update albufeira.tf

--- albufeira.tf
+++ albufeira.tf
@@ -12,7 +12,7 @@ variable "nested_map" {
 locals{
   simple_local_flattened_list = flatten(var.simple_nested_list)
   flattened_list = flatten(var.simple_nested_list)
-  flattened_map  = flatten([for k, v in var.simple_nested_map : [for kk, vv in v : { group = k, key = kk, value = vv }]])
+  flattened_map  = flatten([for k, v in var.nested_map : [for kk, vv in v : { group = k, key = kk, value = vv }]])
   sum_element1=20
   sum_element2=30
 }
