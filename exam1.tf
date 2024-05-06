6014a261aeb5fcbd524a5796f4de913e05d93f03
salomoncloud <158000174+salomoncloud@users.noreply.github.com>
2024-05-06 23:31:34 +0100

    Update exam1.tf

--- exam1.tf
+++ exam1.tf
@@ -1,1 +1,13 @@
+# Define local variables
+locals {
+cloud_providers = ["azure", "aws", "gcp"]
+}
+# Check if 'azure' is in the list and store the result in a local variable
+locals {
+azure_in_list = contains(local.cloud_providers, "azure")
+}
+# Output the result based on the check
+output "check_azure" {
+value = local.azure_in_list ? "Azure is part of the cloud list" : "Azure is not part of the cloud list"
+}
 
