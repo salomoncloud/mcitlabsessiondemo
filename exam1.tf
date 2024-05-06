b95daa5666c695886c9b783e65389d18d7d58a36
salomoncloud <158000174+salomoncloud@users.noreply.github.com>
2024-05-06 23:33:55 +0100

    Update exam1.tf

--- exam1.tf
+++ exam1.tf
@@ -6,6 +6,10 @@ cloud_providers = ["azure", "aws", "gcp"]
 locals {
 azure_in_list = contains(local.cloud_providers, "azure")
 }
+# Output the list of cloud providers
+output "CSP's" {
+value = locals.cloud_providers
+}
 # Output the result based on the check
 output "check_azure" {
 value = local.azure_in_list ? "Azure is part of the cloud list" : "Azure is not part of the cloud list"
