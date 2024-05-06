e3ec6dd8b32df0b3562c009cf2185d2b46a2f721
salomoncloud <158000174+salomoncloud@users.noreply.github.com>
2024-05-07 00:10:24 +0100

    Update exam1.tf

--- exam1.tf
+++ exam1.tf
@@ -3,10 +3,11 @@ locals {
   cloud_owners = ["Microsoft", "Amazon", "Google", "Alibaba Group", "Vanguard Group"]
   cloud_provider_owners = { for index, provider in local.cloud_providers : provider => local.cloud_owners[index] }
   azure_in_list = contains(local.cloud_providers, "azure")
-  flat_sum_exam = [[1,2,3],[1,2],[1,2,3,4],[1,2,3,4,5]]
+  flattened_sum_list = flatten(var.flat_sum_list)
 }
-output "sumoflistinlocals" {
-  value = sum([for numberoutput in local.flat_sum_exam : tonumber(numberoutput)])
+variable "flat_sum_list" {
+  type    = list(list(number))
+  default = [[1,2,3],[1,2],[1,2,3,4],[1,2,3,4,5]]
 }
 output "cloud_providers" {
 value = local.cloud_providers
@@ -16,4 +17,7 @@ value = local.azure_in_list ? "Azure is part of the cloud list" : "Azure is not 
 }
 output "cloud_provider_owners" {
   value = local.cloud_provider_owners
+}
+output "exam_flattened" {
+  value = flatten(var.flat_sum_list)
 }
