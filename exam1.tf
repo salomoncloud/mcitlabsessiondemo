b08996b7efc74a85b5db4a6803ea4c2c468f6996
salomoncloud <158000174+salomoncloud@users.noreply.github.com>
2024-05-07 00:04:36 +0100

    Update exam1.tf

--- exam1.tf
+++ exam1.tf
@@ -3,6 +3,10 @@ locals {
   cloud_owners = ["Microsoft", "Amazon", "Google", "Alibaba Group", "Vanguard Group"]
   cloud_provider_owners = { for index, provider in local.cloud_providers : provider => local.cloud_owners[index] }
   azure_in_list = contains(local.cloud_providers, "azure")
+  flat_sum_exam = [[1,2,3],[1,2],[1,2,3,4],[1,2,3,4,5]]
+}
+output "sumoflistinlocals" {
+  value = sum([for numberoutput in local.flat_sum_exam : tonumber(numberoutput)])
 }
 output "cloud_providers" {
 value = local.cloud_providers
