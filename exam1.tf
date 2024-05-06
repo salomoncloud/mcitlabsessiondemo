f5cb8885fc07ceee08eb5d6e2bf95b8f8bad05aa
salomoncloud <158000174+salomoncloud@users.noreply.github.com>
2024-05-06 23:50:56 +0100

    Update exam1.tf

--- exam1.tf
+++ exam1.tf
@@ -1,10 +1,8 @@
 # Define local variables
 locals {
 cloud_providers = ["azure", "aws", "gcp", "alibaba", "ibm"]
-}
-# Defining the cloud owners variables
-locals {
-cloud_owners=["Microsoft", "Amazon", "Google"]
+cloud_owners = ["Microsoft", "Amazon", "Google", "alibaba group", "vanguard group"]
+cloud_provider_owners = { for index, cloud_providers in local.cloud_providers : cloud_owners => local.cloud_owners[index] }
 }
 # Check if 'azure' is in the list and store the result in a local variable
 locals {
@@ -18,4 +16,7 @@ value = local.cloud_providers
 output "check_azure" {
 value = local.azure_in_list ? "Azure is part of the cloud list" : "Azure is not part of the cloud list"
 }
-
+# Output for the cloud provider owners
+output "cloud_provider_owners" {
+  value = local.cloud_provider_owners
+}
