02cc403491a0ffdcfe073ca4f63261a8d60abf20
salomoncloud <158000174+salomoncloud@users.noreply.github.com>
2024-05-07 01:07:09 +0100

    Update kubernetes.tf

--- kubernetes.tf
+++ kubernetes.tf
@@ -1,3 +1,7 @@
 locals{ 
   cluster_names=["k8batcha01","k9batcha02","k10batcha03","k11batcha04","k12batcha05"]
 }
+resource "azurerm_resource_group" "salomon_rg" {
+  name     = "salomon_resource_group"
+  location = "Canada Central"
+}
