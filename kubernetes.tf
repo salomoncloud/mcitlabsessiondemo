e642b847df12b723181cfe7baf8842ba04f645e0
salomoncloud <158000174+salomoncloud@users.noreply.github.com>
2024-05-07 01:26:22 +0100

    Update kubernetes.tf

--- kubernetes.tf
+++ kubernetes.tf
@@ -5,6 +5,10 @@ resource "azurerm_resource_group" "salomon_rg" {
   name     = var.rg_name
   location = var.location_for_azure
 }
+resource "azurerm_resource_group2" "fritz_rg" {
+  name     = var.rg_name2
+  location = var.location_for_azure2
+}
 variable "location_for_azure" {
 type = string
 default = "Canada East"
@@ -12,4 +16,12 @@ default = "Canada East"
 variable "rg_name" {
 type = string
 default = "salomon_resource_group"
+}
+variable "location_for_azure2" {
+type = string
+default = "West India"
+}
+variable "rg_name2" {
+type = string
+default = "fritz_resource_group"
 }
